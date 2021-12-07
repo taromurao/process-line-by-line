d5026897959f48390dd0dca98fba1934e19d0eb0
Taro Murao <taro.murao@gmail.com>
2021-12-07 21:49:46 +0100

    Use tail library to process new lines added to the observing file.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.debug("Hello");
+import * as path from 'path';
+
+const Tail = require('tail').Tail;
+
+const tail = new Tail(path.join(__dirname, '..', 'test-read-write.txt'));
+
+tail.on("line", function(data: any) {
+  console.log(data);
+});
+
+tail.on("error", function(error: any) {
+  console.log('ERROR: ', error);
+});
+
+function sleep(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+(async () => {
+    while (true) {
+        await sleep(1000);
+    }
+})();
